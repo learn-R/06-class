e7bf681f7cdccd362450c5fe0b6edf2100fa4fef
Nicolás Godoy M <72223972+nicolasggodoym@users.noreply.github.com>
2021-08-19 23:09:12 -0400

    practico 5- covid

---
+++ 04-bloque/R/04-practico.R
@@ -0,0 +1,33 @@
+
+# Práctico 4: Manipulación de tidydata ------------------------------------
+
+
+# 1. Instalación de paquetes ----------------------------------------------
+
+library(pacman)
+pacman::p_load(tidyverse,
+               tidyr,
+               lubridate)
+
+
+# 2. Importar datos ---------------------------------------------------------
+
+datos <- read.csv("https://raw.githubusercontent.com/CSSEGISandData/COVID-19/master/csse_covid_19_data/csse_covid_19_time_series/time_series_covid19_confirmed_global.csv")
+
+# 3. Seleccionar columnas -------------------------------------------------
+
+datos <- datos %>% select(-c(3:4))
+
+
+# 4. pivot_longer() ---------------------------------------------------------
+
+datos <- datos %>% pivot_longer(cols=-(1:2), names_to = "fecha", values_to = "casos")
+
+
+## Bonus: Eliminar caracteres con gsub -------------------------------------
+
+datos$fecha <- gsub("X", "", datos$fecha)
+
+# 5. pivot_wider() --------------------------------------------------------
+
+datos <- datos %>% pivot_wider(names_from = "fecha", values_from = "casos")
